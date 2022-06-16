c898a52c313b43068de4f778f48e236dbec8971f
class_moon <zhaoxin@deallinker.com>
2022-06-16 13:47:03 +0800

    add server

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+
+app.get("/", (_, res) => res.send("hello world!"));
+
+app.listen(port, () =>
+  console.log(`Server runing at http://localhost:${port}/`)
+);
