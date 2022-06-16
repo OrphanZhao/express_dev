e2120916b49b1a29a6bcad18cf9495a89b88c634
class_moon <zhaoxin@deallinker.com>
2022-06-16 13:54:24 +0800

    add nodemon && test post request

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = 4000;
 
 app.get("/", (_, res) => res.send("hello world!"));
 
+app.post("/", (_, res) => res.send("post request!"));
+
 app.listen(port, () =>
   console.log(`Server runing at http://localhost:${port}/`)
 );
