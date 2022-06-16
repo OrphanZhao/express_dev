7803896b1c42ba1c891bab8a35891b5a60781ce6
class_moon <zhaoxin@deallinker.com>
2022-06-16 15:04:48 +0800

    add db.json file

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
+const fs = require("fs");
+
 const express = require("express");
 const app = express();
 const port = 4000;
 
-app.get("/", (_, res) => res.send("hello world!"));
+app.get("/todos", (_, res) => {
+  fs.readFile("./db.json", "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        error: err.message,
+      });
+    }
+    const db = JSON.parse(data);
+    res.status(200).json(db.todos);
+  });
+});
 
-app.post("/", (_, res) => res.send("post request!"));
+app.get("/todos/:id", (req, res) => {
+  fs.readFile("./db.json", "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        error: err.message,
+      });
+    }
+    const db = JSON.parse(data);
+    const item = db.todos.find((v) => v.id == req.params.id) || {};
+    res.status(200).json(item);
+  });
+});
 
 app.listen(port, () =>
   console.log(`Server runing at http://localhost:${port}/`)
